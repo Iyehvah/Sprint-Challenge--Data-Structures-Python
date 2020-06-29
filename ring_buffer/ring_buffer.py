093dc2e28aa7d9fa102bd61ca5767c0b33a216fa
Iyehvah <Iyehvah@users.noreply.github.com>
2020-06-28 20:19:18 -0500

    mvp met

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,16 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.current = 0
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        else:
+            # this takes the first index (which is the least used item in our list and replaces it with our new item)
+            self.storage[self.current] = item
+            self.current += 1
+
+        #once we get to the end our of list it resets the index back to 0
+        if self.current == self.capacity:
+            self.current = 0
 
     def get(self):
-        pass
-
-
-
-
-#ring buffer
-#should delete the least used item in the list if the list is full
-#if the list is not full add the item to the head of the list.
+        return self.storage
